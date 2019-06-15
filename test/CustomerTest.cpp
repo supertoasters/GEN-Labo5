bea2c4995d4d4622d0b5e6b0ab3e9db62604fa08
Caroline <caroline.monthoux@heig-vd.ch>
2019-06-15 17:50:52 +0200

    enable simple test

--- test/CustomerTest.cpp
+++ test/CustomerTest.cpp
@@ -7,21 +7,21 @@
 #include "../src/Movie.h"
 #include "../src/Rental.h"
 
-//TEST(CustomerTest, simple) {
-//    const std::string CUSTOMER_NAME = "Darth Vader";
-//    const std::string MOVIE_NAME = "The Phantom Menace";
-//    const size_t DAYS_RENTED = 34;
-//    const size_t AMOUNT = 2 + (DAYS_RENTED - 2) * 1.5;
-//
-//    Customer customer(CUSTOMER_NAME);
-//    customer.addRental( Rental( Movie( MOVIE_NAME, Movie::REGULAR ), DAYS_RENTED));
-//
-//    std::string result = "Rental Record for " + CUSTOMER_NAME + "\n\t" + MOVIE_NAME + '\t' + std::to_string(AMOUNT) + "\nAmount owed is " + std::to_string(AMOUNT) + "\nYou earned 1 frequent renter points";
-//
-//    ASSERT_EQ(customer.statement(),
-//                 result
-//    );
-//}
+TEST(CustomerTest, simple) {
+    const std::string CUSTOMER_NAME = "Darth Vader";
+    const std::string MOVIE_NAME = "The Phantom Menace";
+    const size_t DAYS_RENTED = 34;
+    const size_t AMOUNT = 2 + (DAYS_RENTED - 2) * 1.5;
+
+    Customer customer(CUSTOMER_NAME);
+    customer.addRental( Rental( Movie( MOVIE_NAME, Movie::REGULAR ), DAYS_RENTED));
+
+    std::string result = "Rental Record for " + CUSTOMER_NAME + "\n\t" + MOVIE_NAME + '\t' + std::to_string(AMOUNT) + "\nAmount owed is " + std::to_string(AMOUNT) + "\nYou earned 1 frequent renter points";
+
+    ASSERT_EQ(customer.statement(),
+                 result
+    );
+}
 
 
 

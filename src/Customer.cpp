044e03198f9a48aea5a3f9d6f1a4d52e76177601
Caroline <caroline.monthoux@heig-vd.ch>
2019-06-16 18:31:12 +0200

    moves iterators directly into the for loop (statement method)

--- src/Customer.cpp
+++ src/Customer.cpp
@@ -23,12 +23,10 @@ std::string Customer::getName() const {
 string Customer::statement() {
     double totalAmount = 0;
     int frequentRenterPoints = 0;
-    vector<Rental>::iterator iter = _rentals.begin();
-    vector<Rental>::iterator iter_end = _rentals.end();
     ostringstream result;
     result << "Rental Record for " << getName() << "\n";
 
-    for (; iter != iter_end; ++iter) {
+    for (auto iter = _rentals.begin(); iter != _rentals.end(); ++iter) {
         Rental thisRental = *iter;
 
         /* Determine amounts for each line */
